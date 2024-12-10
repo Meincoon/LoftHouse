519940592fa842e19e7d21c2c73ba53dbcdaec20
Maksim N <r.riddik1703@yandex.ru>
2024-12-10 23:49:33 +0400

    modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Эмуляция __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -12,7 +11,7 @@ export default defineConfig({
   base: '/LoftHouse/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Теперь '@' указывает на папку src
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
